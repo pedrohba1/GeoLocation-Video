6507f9c588137c77912e529760492182db2b7704
Pedro Bufulin <pedrohba18@gmail.com>
2020-05-24 20:57:37 -0300

    initial configuration, server working

---
+++ backend/src/server/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import Youch from "youch";
+
+import routes from "./routes";
+
+class App {
+    constructor() {
+        this.server = express();
+
+        this.routes();
+        this.exceptionHandler();
+    }
+
+    routes() {
+        this.server.use(routes);
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            if (process.env.NODE_ENV === "development") {
+                const errors = await new Youch(err, req).toJSON();
+                return res.status(500).json(errors);
+            }
+            return res.status(500).json({ error: "internal server error" });
+        });
+    }
+}
+
+export default new App().server;
